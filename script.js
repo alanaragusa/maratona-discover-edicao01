33ca2ea5a2701acdcef2ccb4ff5f2b948dfc7597
Alana Ragusa <alana.ragusarabello@gmail.com>
2021-09-22 20:37:17 +0000

    edited js currency

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const DOM = {
     const html = ` 
     <tr>
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg " alt="Remover transação">
@@ -82,10 +82,17 @@ const DOM = {
 const Utils = {
   formatCurrency(value){
     const signal = Number(value) < 0 ? "-" : ""
+    value = String(value).replace(/\D/g, "")
+    value = Number(value)/100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return signal + value
   }
 }
 
 transactions.forEach(function(transaction){
   DOM.addTransaction(transaction)
 })
-
